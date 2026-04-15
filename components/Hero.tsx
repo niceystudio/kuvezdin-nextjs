a92f7c8adc25e77e44bb9c0a74552516e14c589b
babarogicp <babarogicp@gmail.com>
2026-04-15 13:27:57 +0200

    hero balans2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -76,8 +76,8 @@ export default function Hero() {
         </div>
       </section>
 
-      <section className="hidden min-h-[70vh] md:grid md:max-h-[750px] md:grid-cols-2 lg:grid-cols-[40%_60%] xl:grid-cols-[35%_65%]">
-        <div className="relative flex flex-col justify-center overflow-hidden bg-[#F5EDD8] px-8 py-16 md:px-12 lg:px-16">
+      <section className="hidden min-h-[70vh] md:grid md:max-h-[750px] md:grid-cols-2">
+        <div className="relative flex flex-col items-center justify-center overflow-hidden bg-[#F5EDD8] px-8 py-16 md:px-12 lg:px-16">
           <div
             className="pointer-events-none absolute inset-0 opacity-[0.06]"
             style={{
